d53cf8af39a6f9e6c3e37192774ab97150cac0d9
waxes27 <nceba.dumasi@gmail.com>
2022-05-11 00:50:31 +0200

    geckodriver now

--- main.py
+++ main.py
@@ -11,16 +11,20 @@ import time
 from selenium.webdriver.common.keys import Keys
 from sqlalchemy import true
 from termcolor import colored, cprint
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options
 
 options = Options()
 options.add_argument('--headless')
 options.add_argument('--disable-gpu')  # Last I checked this was necessary.
+options.add_argument('--no-sandbox')         
+# options.add_argument('--disable-dev-shm-usage')        
+
+
 # driver = webdriver.Chrome(CHROMEDRIVER_PATH, chrome_options=options)
 
 
 
-PATH = "driver/chromedriver"
+PATH = "driver/geckodriver"
 # driver = webdriver.Chrome(PATH)
 
 # driver.get("https://businesstech.co.za/news/trending")
@@ -39,15 +43,14 @@ def get_user_preference():
 def display_stories(user_in, articles):
     cprint(f"\n\nNews in ---> {user_in.upper()} <--- News in","green",attrs=['reverse', 'blink'])
     for i in articles:
-        print(f"\n{articles.index(i)+1}:\n{i.text}\n")
+        cprint(f"\n{articles.index(i)+1}:",'green')
+        print(f"\n{i.text}\n")
 
-    # print(f"\nREAD MORE HERE: {i.find_element_by_class_name('post-thumbnail').get_attribute('href')}\n")
-    # time.sleep(3)
 
 def main():
     user_in,link = get_user_preference()
 
-    driver = webdriver.Chrome(PATH,options=options)
+    driver = webdriver.Firefox(executable_path=PATH, options=options)
     driver.get(link)
 
     articles = driver.find_elements_by_tag_name("article")
@@ -62,7 +65,16 @@ def main():
 
 
         user_story = input("Which numbered story would you like to read...[?]: ")
-        driver.get(articles[int(user_story)-1].find_element_by_class_name('post-thumbnail').get_attribute('href'))
+        os.system("clear")
+        cprint(f"\n\n...[ Start of story ]...\n\n",'green')
+        try:
+            time.sleep(3)
+            driver.get(articles[int(user_story)-1].find_element_by_class_name('post-thumbnail').get_attribute('href'))
+        except:
+            print("Ran into issues. Retrying...")
+            time.sleep(3)
+            driver.get(articles[int(user_story)-1].find_element_by_class_name('post-thumbnail').get_attribute('href'))
+
         news = driver.find_element_by_class_name('entry-content').text
 
         news_formatted = "\n".join(news.split("."))
@@ -70,7 +82,12 @@ def main():
         print(news_formatted)
         cprint(f"\n\n...[ End of story ]...\n\n",'red')
         input("...Press Enter for list of news...")
-        driver.back()
+        # try:
+        #     driver.back()
+        # except:
+        #     print("Ran into issues. Retrying...")
+        #     time.sleep(3)
+        #     driver.back()
 
 
 
